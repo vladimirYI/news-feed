21c25e8455a839ba024c8ccf96628d26c2b8fd0c
Vladimir <torna7@yandex.ru>
2021-02-20 17:52:33 +0300

    Use React Router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Header from '../header';
 import NewsItems from '../newsItems';
 import Footer from '../footer';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import style from './App.module.css';
 
 function App() {
   return (
-    <div className={style.app}>
-      <Header categories = {['Category 1', 'Category 2', 'Category 3']} />
-      <NewsItems/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className={style.app}>
+        <Header categories = {['Category 1', 'Category 2', 'Category 3']} />
+        <Route path='/' exact component={() => <h2>Welcome to News-Feed</h2>}/>
+        <Route path='/news' component={NewsItems}/>
+        <Route path='/news' component={Footer}/>
+      </div>
+    </Router>
   );
 }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 import style from './Header.module.css';
 export default function Header(props) {
     let listCategory = props.categories.map((category, index) => 
-        <li className={style.header__items} key={index}><a href="#"> {category} </a></li>
+        <li className={style.header__items} key={index}><Link to='/news'> {category} </Link></li>
     );
     return (
         <div className={style.header}>
-            <a className={style.header__logo} href="#">logo</a>
+            <Link className={style.header__logo} to='/#'>logo</Link>
             <ul className={style.header__categories}>{listCategory}</ul>
         </div>
         
